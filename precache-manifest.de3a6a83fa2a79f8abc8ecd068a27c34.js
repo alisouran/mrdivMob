e630200b867b6b7acd02944240606d482c715cb2
alisouran <alirezasoran@gmail.com>
2020-04-09 09:57:22 +0430

    Updates

---
+++ precache-manifest.de3a6a83fa2a79f8abc8ecd068a27c34.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2b4992a667fccccec1be39631d7e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79663d0efe58ae31548e",
+    "url": "/static/css/main.e41fe9ee.chunk.css"
+  },
+  {
+    "revision": "09f86422f7dc02a2d85e",
+    "url": "/static/js/2.4835165f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4835165f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79663d0efe58ae31548e",
+    "url": "/static/js/main.35490d56.chunk.js"
+  },
+  {
+    "revision": "47f929ba0b4186b4ecaf",
+    "url": "/static/js/runtime-main.5422ca1a.js"
+  },
+  {
+    "revision": "29248d797df5499a3b8b51d791eed99c",
+    "url": "/static/media/02.29248d79.jpg"
+  },
+  {
+    "revision": "c7b79d736112c48d64b3f2214426c388",
+    "url": "/static/media/404.c7b79d73.jpg"
+  },
+  {
+    "revision": "f0ea7661d43e61212ba6c288adcdab77",
+    "url": "/static/media/HomePage.f0ea7661.svg"
+  },
+  {
+    "revision": "32a9365c444760242e313a6c0ee5e76a",
+    "url": "/static/media/Vazir-Bold.32a9365c.woff"
+  },
+  {
+    "revision": "568d89450f813dfc78bd1d0fa948f573",
+    "url": "/static/media/Vazir-Bold.568d8945.eot"
+  },
+  {
+    "revision": "792a144ff1ad9229e395f84859f84bad",
+    "url": "/static/media/Vazir-Bold.792a144f.woff2"
+  },
+  {
+    "revision": "e1fa9df04f68b6c11ba73b2cb2c32111",
+    "url": "/static/media/Vazir-Bold.e1fa9df0.ttf"
+  },
+  {
+    "revision": "63dc8aed0230d5693186695dff5fa598",
+    "url": "/static/media/Vazir.63dc8aed.ttf"
+  },
+  {
+    "revision": "78fb045cc70b332ad0eca7f78031acf3",
+    "url": "/static/media/Vazir.78fb045c.woff"
+  },
+  {
+    "revision": "c97b654993f7c62053b0d2b6505b60f2",
+    "url": "/static/media/Vazir.c97b6549.woff2"
+  },
+  {
+    "revision": "e5b0de94c692df249d1903e3f4412289",
+    "url": "/static/media/Vazir.e5b0de94.eot"
+  },
+  {
+    "revision": "5576479c1a81f5800504d1f300391036",
+    "url": "/static/media/avatar.5576479c.jpg"
+  },
+  {
+    "revision": "97e4691b919f5417bd6dff33f2d9fbd7",
+    "url": "/static/media/news.97e4691b.png"
+  },
+  {
+    "revision": "a43b61d5b7713a3b74e5c34c38a0aa68",
+    "url": "/static/media/react.a43b61d5.png"
+  }
+]);
